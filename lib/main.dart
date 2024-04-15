a34a74a4276e62e0fdf5c4c40860aeedfb1761da
Guilherme Felex <guilherme.felex@hotmail.com>
2024-04-15 15:48:55 -0300

    ajuste de permissão da geolocalização

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
+        '/home': (context) => MapPage(),
         '/map': (context) => MapPage()
       },
     );
